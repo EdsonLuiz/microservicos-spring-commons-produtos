4ce4d4a586e9cdf1a81876672d6999a7ad0940cb
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-03-19 09:22:01 -0300

    Create Produto Library to share

---
+++ src/main/java/com/edson/app/commons/models/entities/Produto.java
@@ -0,0 +1,63 @@
+package com.edson.app.commons.models.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "produtos")
+public class Produto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String nome;
+	private Double preco;
+	
+	@Column(name = "created_at")
+	@Temporal(TemporalType.DATE)
+	private Date createdAt;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+}
---
+++ src/test/java/com/edson/app/commons/ServicoCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edson.app.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
